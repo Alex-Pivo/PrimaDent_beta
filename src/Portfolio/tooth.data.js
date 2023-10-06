5a309dc2f19ad489f60055041828e7e3fb7f2e58
AlexseyPivoshencko <pivoshenckoalexsey@gmail.com>
2023-10-06 12:13:54 +0300

    fix potrfolio and tooth on the main page

--- src/Portfolio/tooth.data.js
+++ src/Portfolio/tooth.data.js
@@ -1,25 +1,25 @@
 export const toothes = [
   {
     id: 1,
-    name: "1. Відбілювання зубів",
+    name: "1. Зміна посмішки за допомогою високоякісних цирконієвих коронок.",
     pathLeft: "../src/4Block/tooth/2.JPG",
     pathRight: "../src/4Block/tooth/11.jpg",
   },
   {
     id: 2,
-    name: "2. Відбілювання зубів",
+    name: "2. Відновлення зубного ряду на верхній щелепі за допомогою дентальних імплантатів по методиці All-on-4 та цирконієвих коронок на титановій балці.",
     pathLeft: "../src/4Block/tooth/34.jpg",
     pathRight: "../src/4Block/tooth/311.jpg",
   },
   {
     id: 3,
-    name: "3. Відбілювання зубів",
+    name: "3. Відновлення посмішки цирконієвими коронками.",
     pathLeft: "../src/4Block/tooth/6.JPG",
     pathRight: "../src/4Block/tooth/51.jpg",
   },
   {
     id: 4,
-    name: "4. Відбілювання зубів",
+    name: "4. Оновлена натуральна посмішка за допомогою 8 керамічних вінірів у відтінку bleach 3.",
     pathLeft: "../src/4Block/tooth/48.jpg",
     pathRight: "../src/4Block/tooth/7.JPG",
   },
